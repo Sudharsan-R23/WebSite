c19afe5c3b30938ce16923c5f33ddfaa711d675a
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-04 14:39:10 +0530

    feact: Adding a buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar'
 import Banner from './assets/B1.png'
 import ROR from './assets/rort1.png'
 import styles from './css/Home.module.css'
+import play from './assets/game.png'
 
 export default function Home() {
   return (
@@ -12,6 +13,16 @@ export default function Home() {
         <img src={Banner} alt="" />
         <div className={styles.RORTitle}>
           <img src={ROR} alt="" />
+        </div>
+        <div className={styles.HomeButton}>
+          <button>Play Mobile</button>
+          <button>Play PC</button>
+        </div>
+        <div className={styles.HomePlay}>
+          <img src={play} alt="" />
+          <img src={play} alt="" />
+          <img src={play} alt="" />
+          <img src={play} alt="" />
         </div>
       </div>
     </div>
