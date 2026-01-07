b333ff87b7217493e280cbd9f190924daa74f131
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-07 21:25:56 +0530

    fix!

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,25 +20,26 @@ export default function () {
         <img src={HeadBanner} className={styles.Banner} alt="" />
         <div className={styles.HeadLayer2}>
           <h1 className={styles.realmTitle}>Revenge of Realms</h1>
+
           <div className={styles.BtnGroup}>
             <button className={`${styles.gameBtn} ${styles.silver}`}>Play Pc</button>
             <button className={`${styles.gameBtn} ${styles.silver}`}>Play Pc</button>
           </div>
           <div className={styles.HomePlay}>
-            <img className={styles.playicon} onClick={() =>{ playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation')} }
+            <img className={styles.playicon} onClick={() => { playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation') }}
               src={steam} alt="" />
-            <img className={styles.playicon} onClick={() =>{ playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation')} }
-              src={epic} alt="" />
-            <img className={styles.playicon} onClick={() =>{ playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation')} }
-              src={ps} alt="" />
-            <img className={styles.playicon} onClick={() =>{ playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation')} }
-              src={xbox} alt="" />
-            
 
+            <img className={styles.playicon} onClick={() => { playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation') }}
+              src={epic} alt="" />
+
+            <img className={styles.playicon} onClick={() => { playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation') }}
+              src={ps} alt="" />
+
+            <img className={styles.playicon} onClick={() => { playNav('https://www.youtube.com/results?search_query=how+to+use+wibsite+link+in+react+navigation') }}
+              src={xbox} alt="" />
           </div>
         </div>
       </section>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem accusantium voluptatibus vitae. Tenetur accusamus sint a maiores nemo, rerum eius ipsa aliquam maxime saepe officia aperiam unde distinctio facilis, ex expedita debitis hic molestias temporibus reiciendis aut. Error quos fugit cupiditate! Illo praesentium dolorem velit molestiae incidunt architecto eos neque officia a, ad repellendus alias fugit enim, aut suscipit corporis quo consequuntur tempore vitae consectetur? Ea eveniet pariatur, distinctio earum expedita, illo placeat quia voluptate beatae sunt, quis animi blanditiis possimus ad. Aliquam saepe sapiente quod, repellat possimus eligendi laborum ipsam excepturi dolore fuga nam dicta magni nostrum vitae quasi culpa earum dolorem at alias quibusdam ut ab? Esse voluptatem, excepturi, minus voluptatibus iure fugit quam possimus accusantium error vel voluptatum adipisci, quod corporis! Error facilis nihil, dolorum illo vitae adipisci eos labore in molestias beatae alias inventore non iste praesentium nulla recusandae quia aut quidem accusamus, cum similique at? Facere quae reiciendis neque nulla provident atque amet dolorem dolore est delectus porro ad eveniet, nobis eaque tempore explicabo consequuntur? Repudiandae vero ut voluptate? Temporibus rerum eaque necessitatibus quae nesciunt illum assumenda ipsam inventore quos eius nemo laboriosam, libero beatae totam incidunt aliquid, cum, corrupti voluptates neque consequatur dolorem provident.</p>
     </>
   )
 }
